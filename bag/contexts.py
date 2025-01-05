184ec2f04905897a811c7907a29d502a4a037af5
Charles Cornfield <ecolus48@gmail.com>
2025-01-05 15:43:54 +0000

    ADD: Begun work on the bag app.

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,18 +1,26 @@
-'''from decimal import Decimal
+from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def bag_contents(request):
 
     bag_items = []
     total = 0
     product_count = 0
+    delivery = 10
+    bag = request.session.get('bag',{})
 
-    if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
-    else:
-        delivery = 0
-        free_delivery_delta = 0
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += 1
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
     
     grand_total = delivery + total
     
@@ -21,9 +29,7 @@ def bag_contents(request):
         'total': total,
         'product_count': product_count,
         'delivery': delivery,
-        'free_delivery_delta': free_delivery_delta,
-        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
     }
 
-    return context'''
+    return context
