b15351111df6fa9d672856b36587c4b4a8bfd751
Charles Cornfield <ecolus48@gmail.com>
2025-01-05 12:41:04 +0000

    Add: Will add product details page to website.

--- products/views.py
+++ products/views.py
@@ -58,7 +58,7 @@ def all_products(request):
 
     return render(request, 'products/products.html', context)
 
-'''def product_detail(request, product_id):
+def product_detail(request, product_id):
     """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
@@ -67,4 +67,4 @@ def all_products(request):
         'product': product,
     }
 
-    return render(request, 'products/product_detail.html', context)'''
+    return render(request, 'products/product_detail.html', context)
