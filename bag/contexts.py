42ecf0bbbb5b72494970d8588aaed0a3f6dd0861
Charles Cornfield <ecolus48@gmail.com>
2025-01-08 12:36:41 +0000

    Add: Fully implemented toasts to project. (Button functionality to be fixed)

--- bag/contexts.py
+++ bag/contexts.py
@@ -4,13 +4,12 @@ from django.shortcuts import get_object_or_404
 from products.models import Product
 
 def bag_contents(request):
-    
-    delivery = 10
 
     bag_items = []
     total = 0
     product_count = 0
     bag = request.session.get('bag', {})
+    delivery = 10
 
     for item_id, quantity in bag.items():
         product = get_object_or_404(Product, pk=item_id)
--- bag/views.py
+++ bag/views.py
@@ -26,14 +26,14 @@ def adjust_bag(request, item_id):
 
 def add_to_bag(request, item_id):
 
-    product = get_object_or_404(Product, pk=item_id)
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
-        messages.success(request, f'updated {product.name} to {bag[item_id]}')
+        messages.success(request, f'Updated {product.name} to {bag[item_id]}')
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {product.name} to your bag')
