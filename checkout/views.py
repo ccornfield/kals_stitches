90276f6f2c68d27dafbda73c0943a4bb19ac8949
Charles Cornfield <ecolus48@gmail.com>
2025-01-09 16:41:38 +0000

    Add: Will begin to implement stripe payment processing.

--- checkout/views.py
+++ checkout/views.py
@@ -1,18 +1,37 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, HttpResponse
 from django.contrib import messages
+from django.conf import settings
+from django.views.decorators.http import require_POST
+
 from .forms import OrderForm
+from bag.contexts import bag_contents
 
 def checkout(request):
     """ A view to return the index page """
+
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag =  request.session.get('bag', {})
     if not bag:
         messages.error(request, "Theres nothing currently in your shopping bag")
         return redirect(reverse('products'))
     
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': stripe_secret_key,
     }
 
     return render(request, template, context)
