5204a854d79e93b55ae5364b2fee7b8e08650237
Charles Cornfield <ecolus48@gmail.com>
2025-01-13 11:24:40 +0000

    Updt: Started working on the contact app.

--- contact/forms.py
+++ contact/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Order
+from .models import Contact
 
 
 class OrderForm(forms.ModelForm):
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    email = models.EmailField(max_length=128, null=False, blank=False )
+    subject = models.CharField(max_length=128, null=False, blank=False)
+    body = models.TextField(null=False, blank=False)
+
+    def __str__(self):
+        return self.subject
