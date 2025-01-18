c6aad79ac83ed89c67ae9fdd11303a6b0da52d8d
Charles Cornfield <ecolus48@gmail.com>
2025-01-18 15:44:15 +0000

    fix: put USE_AWS in '' for testing purposes.

--- kal_stitches/settings.py
+++ kal_stitches/settings.py
@@ -177,14 +177,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-USE_AWS = os.environ.get('USE_AWS')
-
-if USE_AWS in os.environ:
+if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'kals-stitches'
     AWS_S3_REGION_NAME = 'eu-north-1'
