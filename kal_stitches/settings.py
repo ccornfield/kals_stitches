50665eb7d531c094accad8573abe31d8aaad0d4e
Charles Cornfield <ecolus48@gmail.com>
2025-01-22 16:29:57 +0000

    fix: corrected another spelling mistake in settings.py

--- kal_stitches/settings.py
+++ kal_stitches/settings.py
@@ -224,5 +224,5 @@ else:
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
     EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-    EMAIL_HOST_PASS = os.getenv('EMAIL_HOST_PASS')
+    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
